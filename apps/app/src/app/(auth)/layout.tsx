2995752975ee8e9d2239d1384cf956b911b993f2
Leo Giovanetti <hello@leog.me>
2023-12-01 21:09:48 -0300

    fixing login component error

--- apps/app/src/app/(auth)/layout.tsx
+++ apps/app/src/app/(auth)/layout.tsx
@@ -1,25 +1,17 @@
-import Header from '@/components/ui/header'
-
 export default function AuthLayout({
   children,
 }: {
-  children: React.ReactNode
-}) {  
+  children: React.ReactNode;
+}) {
   return (
     <>
-      <Header />
-
       <main className="grow">
-        <section className="relative before:absolute before:inset-0 before:h-80 before:pointer-events-none before:bg-gradient-to-b before:from-zinc-100 before:-z-10">
-          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-            <div className="px-4 sm:px-6">
-
-              {children}
-
-            </div>
+        <section className="relative before:pointer-events-none before:absolute before:inset-0 before:-z-10 before:h-80 before:bg-gradient-to-b before:from-zinc-100">
+          <div className="pb-12 pt-32 md:pb-20 md:pt-40">
+            <div className="px-4 sm:px-6">{children}</div>
           </div>
         </section>
       </main>
     </>
-  )
+  );
 }
--- apps/app/src/app/(auth)/login/page.tsx
+++ apps/app/src/app/(auth)/login/page.tsx
@@ -1,62 +1,97 @@
-export const metadata = {
-  title: 'Log in - Creative',
-  description: 'Page description',
-}
+import Link from "next/link";
 
-import Link from 'next/link'
+export const metadata = {
+  title: "Log in - Creative",
+  description: "Page description",
+};
 
 export default function Login() {
   return (
     <>
       {/* Page header */}
-      <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-        <h1 className="font-inter-tight text-4xl md:text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-zinc-500 via-zinc-900 to-zinc-900">Log in to Gray</h1>
+      <div className="mx-auto max-w-3xl pb-12 text-center md:pb-16">
+        <h1 className="font-inter-tight bg-gradient-to-r from-zinc-500 via-zinc-900 to-zinc-900 bg-clip-text text-4xl font-bold text-transparent md:text-5xl">
+          Log in to Props.to
+        </h1>
       </div>
 
       {/* Form */}
-      <div className="max-w-[25rem] mx-auto p-6 rounded-lg shadow-2xl bg-gradient-to-b from-zinc-100 to-zinc-50/70 relative before:absolute before:-top-12 before:-left-16 before:w-96 before:h-96 before:bg-zinc-900 before:opacity-[.15] before:rounded-full before:blur-3xl before:-z-10">
-
+      <div className="relative mx-auto max-w-[25rem] rounded-lg bg-gradient-to-b from-zinc-100 to-zinc-50/70 p-6 shadow-2xl before:absolute before:-left-16 before:-top-12 before:-z-10 before:h-96 before:w-96 before:rounded-full before:bg-zinc-900 before:opacity-[.15] before:blur-3xl">
         <form>
           <div className="space-y-4">
             <div>
-              <label className="block text-sm text-zinc-800 font-medium mb-2" htmlFor="email">Email</label>
-              <input id="email" className="form-input text-sm w-full" type="email" placeholder="mark@acmecorp.com" required />
+              <label
+                className="mb-2 block text-sm font-medium text-zinc-800"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                className="form-input w-full text-sm"
+                type="email"
+                placeholder="mark@acmecorp.com"
+                required
+              />
             </div>
             <div>
               <div className="flex justify-between">
-                <label className="block text-sm text-zinc-800 font-medium mb-2" htmlFor="password">Password</label>
-                <Link className="text-sm font-medium text-zinc-500 underline hover:no-underline ml-2" href="/reset-password">Forgot?</Link>
+                <label
+                  className="mb-2 block text-sm font-medium text-zinc-800"
+                  htmlFor="password"
+                >
+                  Password
+                </label>
+                <Link
+                  className="ml-2 text-sm font-medium text-zinc-500 underline hover:no-underline"
+                  href="/reset-password"
+                >
+                  Forgot?
+                </Link>
               </div>
-              <input id="password" className="form-input text-sm w-full" type="password" required />
+              <input
+                id="password"
+                className="form-input w-full text-sm"
+                type="password"
+                required
+              />
             </div>
           </div>
           <div className="mt-5">
-            <button className="btn text-zinc-100 bg-zinc-900 hover:bg-zinc-800 w-full shadow">Log in</button>
+            <button className="btn w-full bg-zinc-900 text-zinc-100 shadow hover:bg-zinc-800">
+              Log in
+            </button>
           </div>
         </form>
 
         {/* Divider */}
-        <div className="flex items-center my-5 before:border-t before:border-zinc-200 before:grow before:mr-3 after:border-t after:border-zinc-200 after:grow after:ml-3">
-          <div className="text-xs text-zinc-400 italic">Or</div>
+        <div className="my-5 flex items-center before:mr-3 before:grow before:border-t before:border-zinc-200 after:ml-3 after:grow after:border-t after:border-zinc-200">
+          <div className="text-xs italic text-zinc-400">Or</div>
         </div>
 
         {/* Social login */}
-        <button className="btn text-zinc-600 bg-white hover:text-zinc-900 w-full shadow group relative flex after:flex-1">
-          <div className="flex-1 flex items-center">
-            <svg className="w-4 h-4 fill-zinc-400 group-hover:fill-rose-500 shrink-0 transition" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+        <button className="btn group relative flex w-full bg-white text-zinc-600 shadow after:flex-1 hover:text-zinc-900">
+          <div className="flex flex-1 items-center">
+            <svg
+              className="h-4 w-4 shrink-0 fill-zinc-400 transition group-hover:fill-rose-500"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path d="M15.679 6.545H8.043v3.273h4.328c-.692 2.182-2.401 2.91-4.363 2.91a4.727 4.727 0 1 1 3.035-8.347l2.378-2.265A8 8 0 1 0 8.008 16c4.41 0 8.4-2.909 7.67-9.455Z" />
             </svg>
           </div>
           <span className="flex-auto pl-3">Continue With Google</span>
         </button>
 
-        <div className="text-center mt-6">
+        <div className="mt-6 text-center">
           <div className="text-xs text-zinc-500">
-            By loggin in you agree with our <a className="underline hover:no-underline" href="#0">Terms</a>
+            By loggin in you agree with our{" "}
+            <a className="underline hover:no-underline" href="#0">
+              Terms
+            </a>
           </div>
         </div>
-
       </div>
     </>
-  )
+  );
 }
