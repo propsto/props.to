f1459e4b5785f8814d7d1bb90ed2ef8316da4ae1
Leo Giovanetti <hello@leog.me>
2024-07-28 18:52:30 -0300

    upgrading turbo, fixing theme toggle

--- apps/auth/src/app/_components/theme-toogle.tsx
+++ apps/auth/src/app/_components/theme-toogle.tsx
@@ -2,53 +2,69 @@
 
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import Image from "next/image";
-import { useLongPress } from "@/lib/use-long-press";
+import { SunMoon, Sun, Moon } from "lucide-react";
 
 export function ThemeToggle(): JSX.Element {
   const { theme, setTheme } = useTheme();
   const [changeThemeValue, setChangeThemeValue] = useState<string>("dark");
-  const { longPressHandlers } = useLongPress({
-    onLongPress: () => {
-      setTheme("system");
-    },
-    onClick: () => {
-      setTheme(changeThemeValue);
-    },
-  });
 
   useEffect(() => {
-    // detect if current system theme is dark
-    const checkDarkTheme = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-    // Find new theme value for setTheme
-    let newValue;
-    if (theme === "dark") {
-      newValue = "light";
-    } else if (theme === "light") {
-      newValue = "dark";
-    } else if (checkDarkTheme) {
-      newValue = "light";
-    } else {
-      newValue = "dark";
+    function getNextThemeOption(matches: boolean): void {
+      let newValue;
+      if (theme === "dark") {
+        newValue = "light";
+      } else if (theme === "light") {
+        newValue = "dark";
+      } else if (matches) {
+        newValue = "light";
+      } else {
+        newValue = "dark";
+      }
+      setChangeThemeValue(newValue);
     }
-    setChangeThemeValue(newValue);
+    // detect if current system theme is dark
+    const checkDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+    getNextThemeOption(checkDarkTheme.matches);
+    checkDarkTheme.addEventListener("change", (e: MediaQueryListEvent) => {
+      getNextThemeOption(e.matches);
+    });
+    return () => {
+      checkDarkTheme.removeEventListener("change", (e: MediaQueryListEvent) => {
+        getNextThemeOption(e.matches);
+      });
+    };
   }, [theme]);
+
+  if (typeof window === "undefined") {
+    setChangeThemeValue("system");
+  }
 
   return (
     <button
       type="button"
       className="absolute bottom-4 right-4"
-      {...longPressHandlers}
+      title={
+        changeThemeValue === "dark" && theme !== "system"
+          ? "Switch to system theme"
+          : `Switch to ${theme === "system" ? "dark or light" : changeThemeValue} theme`
+      }
+      onClick={() => {
+        setTheme(
+          changeThemeValue === "dark" && theme !== "system"
+            ? "system"
+            : changeThemeValue
+        );
+      }}
     >
-      <Image
-        alt="theme toggle"
-        width="24"
-        height="24"
-        className="size-6"
-        src={changeThemeValue === "light" ? "sun.svg" : "moon.svg"}
-      />
+      {theme === "system" && (
+        <SunMoon aria-label="System theme" className="size-6" />
+      )}
+      {changeThemeValue === "light" && theme !== "system" && (
+        <Sun aria-label="Light theme" className="size-6" />
+      )}
+      {changeThemeValue === "dark" && theme !== "system" && (
+        <Moon aria-label="Dark theme" className="size-6" />
+      )}
     </button>
   );
 }
