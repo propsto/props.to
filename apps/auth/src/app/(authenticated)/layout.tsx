64b43b849c347cb25a4cb6f8031711714a53a2bf
Leo Giovanetti <hello@leog.me>
2024-12-26 15:54:22 -0300

    fixes

--- apps/auth/src/app/(authenticated)/layout.tsx
+++ apps/auth/src/app/(authenticated)/layout.tsx
@@ -13,14 +13,14 @@ export default function Layout({
   children,
 }: Readonly<React.PropsWithChildren>): React.ReactNode {
   return (
-    <div className="grid lg:grid-cols-3 h-screen py-5">
+    <div className="grid lg:grid-cols-3 h-screen">
       <div className="lg:col-span-2 overflow-y-auto">
         <div className="flex lg:!hidden mb-5 h-12 overflow-hidden justify-center relative items-center text-2xl font-medium font-cal tracking-wider">
           <Triangles size="small" />
           <Logo className="mr-2 z-20" size="large" />
           Props.to
         </div>
-        <div className="min-h-full flex flex-col justify-center items-center">
+        <div className="min-h-full flex flex-col justify-center items-center py-5">
           {children}
           <DynamicThemeToggle />
         </div>
