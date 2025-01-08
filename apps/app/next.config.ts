9b65b6d234cd6105e7732413d663f946d022a7bb
Leo Giovanetti <hello@leog.me>
2025-01-07 23:55:03 -0300

    testing with app

--- apps/app/next.config.ts
+++ apps/app/next.config.ts
@@ -1,4 +1,9 @@
 import { type NextConfig } from "next";
+import dotenvExpand from "dotenv-expand";
+
+dotenvExpand.expand({
+  parsed: { ...process.env } as Record<string, string>,
+});
 
 export default {
   reactStrictMode: true,
