a56b753bfa847527ce5a0760c9b180aa4d1806b0
Leo Giovanetti <hello@leog.me>
2025-01-07 23:13:26 -0300

    adjusting interpolation, logging

--- packages/constants/vercel.ts
+++ packages/constants/vercel.ts
@@ -1,14 +1,17 @@
+/* eslint-disable no-template-curly-in-string -- required by lib */
 import dotenvExpand from "dotenv-expand";
 
 const { parsed } = dotenvExpand.expand({
   parsed: {
-    AUTH_URL: "https://auth-git-$VERCEL_GIT_COMMIT_REF-propsto.vercel.app",
+    AUTH_URL: "https://auth-git-${VERCEL_GIT_COMMIT_REF}-propsto.vercel.app",
     PROPSTO_HOST: "vercel.app",
     PROPSTO_APP_URL:
-      "https://app-git-$VERCEL_GIT_COMMIT_REF-propsto.vercel.app",
+      "https://app-git-${VERCEL_GIT_COMMIT_REF}-propsto.vercel.app",
   },
 });
 
 const out = parsed?.env ?? {};
+// eslint-disable-next-line no-console -- temp
+console.log({ out });
 
 export const env = out;
