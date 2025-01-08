549f37e52cc90bde679f65a0655ed38453055a6b
Leo Giovanetti <hello@leog.me>
2025-01-08 15:24:18 -0300

    console log

--- apps/app/src/middleware.ts
+++ apps/app/src/middleware.ts
@@ -4,6 +4,11 @@ import { auth } from "@/server/auth.edge";
 export default function middleware(request: NextRequest): Response {
   return auth(req => {
     if (!req.auth) {
+      // eslint-disable-next-line no-console -- temp
+      console.log({
+        AUTH_URL: process.env.AUTH_URL,
+        PROPSTO_APP_URL: process.env.PROPSTO_APP_URL,
+      });
       const newUrl = new URL(process.env.AUTH_URL ?? "");
       newUrl.searchParams.set("callbackUrl", request.nextUrl.href);
       return NextResponse.redirect(newUrl.href);
