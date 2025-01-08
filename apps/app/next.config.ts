2f3488a3c919a120bb85aa3b55e105e0246c26be
Leo Giovanetti <hello@leog.me>
2025-01-07 22:20:52 -0300

    using dotenv-expand

--- apps/app/next.config.ts
+++ apps/app/next.config.ts
@@ -1,9 +1,13 @@
 import { type NextConfig } from "next";
+import dotenvExpand from "dotenv-expand";
+
+dotenvExpand.expand({ parsed: { ...process.env } as Record<string, string> });
+
 export default {
   reactStrictMode: true,
 
   /**
-   * If you have the "experimental: { appDir: true }" setting enabled, then you
+   * If you have the "experimental: \{ appDir: true \}" setting enabled, then you
    * must comment the below `i18n` config out.
    *
    * @see https://github.com/vercel/next.js/issues/41980
