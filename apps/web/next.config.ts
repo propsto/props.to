c797e08f27bb2bfd1acd721fa8e073265c5713bd
Leo Giovanetti <hello@leog.me>
2025-11-11 12:49:02 -0300

    Fixing web

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,10 @@
+import { vercelPreviewEnvVars } from "@propsto/constants/other";
 import { type NextConfig } from "next";
 
 export default {
+  env: {
+    ...vercelPreviewEnvVars,
+  },
   experimental: {
     serverActions: {},
   },
